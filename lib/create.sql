f2e420662c1e77e1ef4c3f9767ec01c60c323306
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-25 20:16:18 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,21 @@
-CREATE TABLE project (
-id INTEGER PRIMARY KEY,
-title TEXT,
-category TEXT,
-)
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount REAL,
+  user_id INTEGER,
+  project_id INTEGER
+);
